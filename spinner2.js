ab68aff44d1653efaf76cc5d7071c8fbc631146f
Shajee-j <Shajeejm@gmail.com>
2023-07-07 02:52:05 -0400

    add ability to 'pass' number of spins

--- spinner2.js
+++ spinner2.js
@@ -1,15 +1,31 @@
-// ver 1 : refractored, but results in only a single spin
+// ver 1 : refactored, but results in only a single spin
 
 
-const x = (str) => {
-  let time = 100
-  for (let sym of str) {
-    time += 200
-    setTimeout(() => {
-      process.stdout.write(`\r${sym}   `);
-    }, time);
-  }
+// const x = (str) => {
+//   let time = 100
+//   for (let sym of str) {
+//     time += 200
+//     setTimeout(() => {
+//       process.stdout.write(`\r${sym}   `);
+//     }, time);
+//   }
+// };
+
+
+
+// ver 2: improved upon ver 1; enabled the ability to choose how many spins the code will result in by passing in a "num"  value as a 2nd param. 
+
+const x = (str, num) => {
+  let time = 100;
+  for (let i = 0; i < num; i++) {
+    for (let sym of str) {
+      time += 200;
+      setTimeout(() => {
+        process.stdout.write(`\r${sym}   `);
+      }, time);
+    };
+  };
 };
 
 
-x("|/-\\|")
+x("|/-\\|", 5)
